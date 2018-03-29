3e2954468606a59d6c193dccb79eba4721c98d77
Fr3ud <oo.pavelko@gmail.com>
2018-03-29 17:19:40 +0300

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apavelko <apavelko@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/13 14:24:00 by apavelko          #+#    #+#             */
+/*   Updated: 2018/01/16 13:08:49 by apavelko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <string.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
